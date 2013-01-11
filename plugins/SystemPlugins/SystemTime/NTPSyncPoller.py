25ac8a8ecf67db33d46252ed0b9c2e5009e3cde0
root <root@vdruser.myhome.ru>
2013-01-11 12:56:37 +0400

    Little change in plugins SystemTime

--- plugins/SystemPlugins/SystemTime/NTPSyncPoller.py
+++ plugins/SystemPlugins/SystemTime/NTPSyncPoller.py
@@ -23,4 +23,6 @@ class NTPSyncPoller:
 	def NTPStart(self):
 		if config.plugins.SystemTime.choiceSystemTime.value == "1":
 			self.Console.ePopen('/usr/bin/ntpdate -b -s -u pool.ntp.org')
+		else:
+			self.Console.ePopen('/usr/bin/dvbdate -p -s -f')
 		self.timer.startLongTimer(int(config.plugins.SystemTime.useNTPminutes.value) * 60)
