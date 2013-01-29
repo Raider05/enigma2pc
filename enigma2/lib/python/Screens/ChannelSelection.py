0ad0f44b90b51a9ef8c76b4ac7169d1dd1b9c4bd
root <root@vdruser.myhome.ru>
2013-01-29 12:56:27 +0400

    merge - solved crash for hide channel list in radio mode

--- enigma2/lib/python/Screens/ChannelSelection.py
+++ enigma2/lib/python/Screens/ChannelSelection.py
@@ -1617,6 +1617,7 @@ class ChannelSelectionRadio(ChannelSelectionBase, ChannelSelectionEdit, ChannelS
 		ChannelSelectionEPG.__init__(self)
 		InfoBarBase.__init__(self)
 		self.infobar = infobar
+		self.startServiceRef = None
 		self.onLayoutFinish.append(self.onCreate)
 
 		self.info = session.instantiateDialog(RadioInfoBar) # our simple infobar
@@ -1746,6 +1747,9 @@ class ChannelSelectionRadio(ChannelSelectionBase, ChannelSelectionEdit, ChannelS
 					config.radio.lastservice.value = ref.toString()
 					config.radio.lastservice.save()
 				self.saveRoot()
+
+	def zapBack(self):
+		self.channelSelected()
 
 class SimpleChannelSelection(ChannelSelectionBase):
 	def __init__(self, session, title):
