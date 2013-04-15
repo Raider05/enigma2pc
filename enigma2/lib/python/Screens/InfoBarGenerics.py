8bf3176a6a0c208efe0cfac600c0454b9c76527d
root <root@vdruser.myhome.ru>
2013-04-15 16:19:44 +0400

    merge - InfoBarGenerics: Initialize TimeshiftActivateActions Inactive

--- enigma2/lib/python/Screens/InfoBarGenerics.py
+++ enigma2/lib/python/Screens/InfoBarGenerics.py
@@ -1387,6 +1387,7 @@ class InfoBarTimeshift:
 			}, prio=-1) # priority over record
 
 		self.timeshift_enabled = False
+		self["TimeshiftActivateActions"].setEnabled(False)
 		self.ts_rewind_timer = eTimer()
 		self.ts_rewind_timer.callback.append(self.rewindService)
 
