11fd629f19410c2aa8ef9692c0aa7ca37429f41b
root <root@vdruser.myhome.ru>
2013-04-16 09:29:41 +0400

    merge - InfoBar/InfoBarGenerics: Enable TIMER Button on RemoteControl

--- enigma2/lib/python/Screens/InfoBarGenerics.py
+++ enigma2/lib/python/Screens/InfoBarGenerics.py
@@ -2041,6 +2041,17 @@ class InfoBarRedButton:
 			for x in self.onRedButtonActivation:
 				x()
 
+class InfoBarTimerButton:
+	def __init__(self):
+		self["TimerButtonActions"] = HelpableActionMap(self, "InfobarTimerButtonActions",
+			{
+				"timerSelection": (self.timerSelection, _("Timer selection...")),
+			})
+
+	def timerSelection(self):
+		from Screens.TimerEdit import TimerEditList
+		self.session.open(TimerEditList)
+
 class InfoBarAdditionalInfo:
 	def __init__(self):
 
