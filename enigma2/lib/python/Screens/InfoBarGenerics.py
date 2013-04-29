4885a5160faafc0e44a8384e3aa44eabc1c0297f
root <root@vdruser.myhome.ru>
2013-04-29 16:21:39 +0400

    merge - InfoBarGenerics: simplify a bit of code

--- enigma2/lib/python/Screens/InfoBarGenerics.py
+++ enigma2/lib/python/Screens/InfoBarGenerics.py
@@ -2026,9 +2026,7 @@ class InfoBarInstantRecord:
 					for x in self.recording:
 						if x.isRunning() and x == timer:
 							identical += 1
-		if timers > identical:
-			return True
-		return False
+		return timers > identical
 
 	def instantRecord(self):
 		dir = preferredInstantRecordPath()
