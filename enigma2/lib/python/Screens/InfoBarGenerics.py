14277efb4750006d0f4d0369642d97df2199725f
root <root@vdruser.myhome.ru>
2013-06-16 11:57:58 +0400

    merge - Show also the instant recordings when in movie infobar

--- enigma2/lib/python/Screens/InfoBarGenerics.py
+++ enigma2/lib/python/Screens/InfoBarGenerics.py
@@ -1919,7 +1919,13 @@ class InfoBarInstantRecord:
 			{
 				"instantRecord": (self.instantRecord, _("Instant recording...")),
 			})
-		self.recording = []
+		if isStandardInfoBar(self):
+			self.recording = []
+		else:
+			from Screens.InfoBar import InfoBar
+			InfoBarInstance = InfoBar.instance
+			if InfoBarInstance:
+				self.recording = InfoBarInstance.recording
 
 	def stopCurrentRecording(self, entry = -1):
 		if entry is not None and entry != -1:
