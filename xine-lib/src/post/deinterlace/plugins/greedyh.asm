454cb0a7ed18af84d80ea09a3fddcf208203a72a
root <root@coolcomp.(none)>
2013-09-04 15:50:40 +0400

    merge xine-lib-1.2.3:  Fix FSF address in a bunch of files

--- xine-lib/src/post/deinterlace/plugins/greedyh.asm
+++ xine-lib/src/post/deinterlace/plugins/greedyh.asm
@@ -7,7 +7,8 @@
 //	published by the Free Software Foundation.  A copy of this license is
 //	included with this software distribution in the file COPYING.  If you
 //	do not have a copy, you may obtain a copy by writing to the Free
-//	Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//	Software Foundation, 51 Franklin St, Fifth Floor, Boston, MA 
+//  02110-1301, USA.
 //
 //	This software is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
