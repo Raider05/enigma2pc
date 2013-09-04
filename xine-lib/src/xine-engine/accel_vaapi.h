d40d3fcd4119c56aae2793482cb23c45a49babd4
root <root@coolcomp.(none)>
2013-09-04 15:49:36 +0400

    merge xine-lib-1.2.3:  fix comment

--- xine-lib/src/xine-engine/accel_vaapi.h
+++ xine-lib/src/xine-engine/accel_vaapi.h
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
  *
  *
- * Common acceleration definitions for vdpau
+ * Common acceleration definitions for vaapi
  *
  *
  */
