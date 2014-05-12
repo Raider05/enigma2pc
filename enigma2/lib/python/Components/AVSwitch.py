7659916641d8c5ef18f2746ee88a39173b61901f
root <root@coolcomp.(none)>
2014-05-12 11:18:07 +0400

    merge - Revert 'Move VRC Scart on main menu option to Videomode (av) menu'

--- enigma2/lib/python/Components/AVSwitch.py
+++ enigma2/lib/python/Components/AVSwitch.py
@@ -149,7 +149,6 @@ def InitAVSwitch():
 
 	iAVSwitch.setInput("ENCODER") # init on startup
 	SystemInfo["ScartSwitch"] = eAVSwitch.getInstance().haveScartSwitch()
-	config.av.show_vcr_scart = ConfigEnableDisable(default = False)
 
 	try:
 		can_downmix = open(eEnv.resolve("${sysconfdir}/stb/audio/ac3_choices"), "r").read()[:-1].find("downmix") != -1
