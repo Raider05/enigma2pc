239d6387d100e4a8b468b8061fa8b2a2f2db26d9
root <root@coolcomp.(none)>
2014-05-16 11:00:45 +0400

    merge - Another finetuning regards deepstandby and record timers

--- enigma2/RecordTimer.py
+++ enigma2/RecordTimer.py
@@ -354,6 +354,8 @@ class RecordTimerEntry(timer.TimerEntry, object):
 
 	def keypress(self, key=None, flag=1):
 		if flag and self.wasInStandby:
+			if not config.misc.standbyCounter.value:
+				config.misc.standbyCounter.value += 1
 			self.wasInStandby = False
 			eActionMap.getInstance().unbindAction('', self.keypress)
 
