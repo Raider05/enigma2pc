e096b325d391952ff3a7458a3026c7c23683a4ff
root <root@coolcomp.(none)>
2014-05-29 17:42:18 +0400

    merge - ScanSetup: Reassing OK button to start the scan

--- enigma2/lib/python/Screens/ScanSetup.py
+++ enigma2/lib/python/Screens/ScanSetup.py
@@ -327,6 +327,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 
 		self["actions"] = NumberActionMap(["SetupActions", "MenuActions"],
 		{
+			"ok": self.keyGo,
 			"save": self.keyGo,
 			"cancel": self.keyCancel,
 			"menu": self.doCloseRecursive,
