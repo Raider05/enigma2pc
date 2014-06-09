6a99e5178568c821ad78345aaac40194e6a1cd36
root <root@coolcomp.(none)>
2014-06-09 10:09:22 +0400

    merge - StartWizard: use HardwareInfo()

--- enigma2/lib/python/Screens/StartWizard.py
+++ enigma2/lib/python/Screens/StartWizard.py
@@ -1,8 +1,7 @@
 from Wizard import wizardManager
 from Screens.WizardLanguage import WizardLanguage
-from Screens.ScanSetup import DefaultSatLists
-from Screens.DefaultWizard import DefaultWizard
 from Screens.Rc import Rc
+from Tools.HardwareInfo import HardwareInfo
 
 from Components.Pixmap import Pixmap, MovingPixmap, MultiPixmap
 from Components.config import config, ConfigBoolean, configfile, ConfigSubsection
@@ -10,23 +9,26 @@ from Components.config import config, ConfigBoolean, configfile, ConfigSubsectio
 from LanguageSelection import LanguageWizard
 
 config.misc.firstrun = ConfigBoolean(default = True)
-config.misc.startwizard = ConfigSubsection()
-config.misc.startwizard.shownimconfig = ConfigBoolean(default = True)
-config.misc.startwizard.doservicescan = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
 
-class StartWizard(DefaultSatLists, Rc):
+class StartWizard(WizardLanguage, Rc):
 	def __init__(self, session, silent = True, showSteps = False, neededTag = None):
-		self.xmlfile = ["startwizard.xml", "defaultsatlists.xml"]
+		self.xmlfile = ["startwizard.xml"]
 		WizardLanguage.__init__(self, session, showSteps = False)
-		DefaultWizard.__init__(self, session, silent, showSteps, neededTag = "services")
 		Rc.__init__(self)
 		self["wizard"] = Pixmap()
-				
+
 	def markDone(self):
+		# setup remote control, all stb have same settings except dm8000 which uses a different settings
+		if HardwareInfo().get_device_name() == 'dm8000':
+			config.misc.rcused.value = 0
+		else:
+			config.misc.rcused.value = 1
+		config.misc.rcused.save()
+
 		config.misc.firstrun.value = 0
 		config.misc.firstrun.save()
 		configfile.save()
-		
+
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority = 5)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority = 20)
